910b7815b8f2c68fce517616bcff2f6c912ad94c
AkosSeres <seresakos2000@gmail.com>
2022-10-29 20:05:18 +0200

    Add more objects to the scene

--- src/lib/ThreeCanvas.svelte
+++ src/lib/ThreeCanvas.svelte
@@ -18,16 +18,37 @@
 
         const scene = new THREE.Scene();
 
-        const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-        const material = new THREE.MeshNormalMaterial();
+        const geometry = new THREE.BoxGeometry(2, 0.2, 2);
+        const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 
         const mesh = new THREE.Mesh(geometry, material);
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
         scene.add(mesh);
+
+        const box = new THREE.Mesh(
+            new THREE.BoxGeometry(0.2, 0.2, 0.2),
+            new THREE.MeshPhongMaterial({ color: 0xaa11aa })
+        );
+        box.translateY(0.2);
+        box.castShadow = true;
+        box.receiveShadow = true;
+        scene.add(box);
+
+        scene.add(new THREE.AmbientLight(0x404040));
+        const sun = new THREE.DirectionalLight(0xffffff, 0.6);
+        sun.position.set(-1, 1, 1);
+        sun.castShadow = true;
+        sun.shadow.mapSize.width = 1024;
+        sun.shadow.mapSize.height = 1024;
+        scene.add(sun);
 
         const renderer = new THREE.WebGLRenderer({
             antialias: true,
             canvas,
         });
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         renderer.setAnimationLoop(animation);
 
         const resize = () => {
@@ -44,6 +65,7 @@
 
         // animation
         function animation(time) {
+            controls.update();
             renderer.render(scene, camera);
         }
     });
