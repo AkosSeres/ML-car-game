9e6270ee0c1eeefb4ee141cf6e5285525f3c35d9
AkosSeres <seresakos2000@gmail.com>
2022-10-31 20:04:30 +0100

    Implement tab switches

--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -1,24 +1,54 @@
 <script>
-    import { Tabs, TabItem } from "flowbite-svelte";
     import DrawPanel from "./DrawPanel.svelte";
     import PlayPanel from "./PlayPanel.svelte";
     import TrainPanel from "./TrainPanel.svelte";
+    import { DrawMode } from "./DrawMode";
+    import { PlayMode } from "./PlayMode";
+    import { TrainMode } from "./TrainMode";
+    import { gameWorldStore } from "./stores";
+
+    const defaultTab = 2;
+    const titles = ["Draw", "Play", "Train"];
+    const panels = [DrawPanel, PlayPanel, TrainPanel];
+    /** @type {import("./Mode").Mode[]} */
+    const modes = [];
+
+    let activeTab = -1;
+    let gameWorld;
+
+    gameWorldStore.subscribe((gameWorldNew) => {
+        if (gameWorldNew) {
+            gameWorld = gameWorldNew;
+            modes.push(new DrawMode(gameWorld));
+            modes.push(new PlayMode(gameWorld));
+            modes.push(new TrainMode(gameWorld));
+            setActiveTab(defaultTab);
+        }
+    });
+
+    function setActiveTab(num) {
+        if (activeTab === num) return;
+        if (activeTab !== -1) modes[activeTab].deactivate();
+        activeTab = num;
+        modes[activeTab].activate();
+    }
 </script>
 
-<Tabs
-    style="full"
-    defaultClass="flex rounded-lg divide-x divide-gray-200 shadow dark:divide-gray-700"
->
-    <TabItem class="w-full" open>
-        <span slot="title">Draw</span>
-        <DrawPanel />
-    </TabItem>
-    <TabItem class="w-full">
-        <span slot="title">Play</span>
-        <PlayPanel />
-    </TabItem>
-    <TabItem class="w-full">
-        <span slot="title">Train</span>
-        <TrainPanel />
-    </TabItem>
-</Tabs>
+<div class="inline-flex rounded-md shadow-sm w-full" role="group">
+    {#each titles as title, i}
+        <button
+            on:click={() => setActiveTab(i)}
+            type="button"
+            class="transition-all grow py-2 px-4 text-md {i === 0
+                ? 'rounded-l-lg'
+                : ''} {i === titles.length - 1 ? 'rounded-r-lg' : ''} {i ===
+            activeTab
+                ? 'font-bold text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'
+                : 'font-medium border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white text-gray-900 bg-white'} focus:z-10"
+        >
+            {title}
+        </button>
+    {/each}
+</div>
+
+<svelte:component this={panels[activeTab]} />
