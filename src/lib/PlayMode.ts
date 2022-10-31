179f4cdeeb8aa5dcefc37fe690e1fd1f6cb9ed58
AkosSeres <seresakos2000@gmail.com>
2022-10-31 20:55:34 +0100

    Add controllable car to play mode

--- src/lib/PlayMode.ts
+++ src/lib/PlayMode.ts
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { GameWorld } from "./GameWorld";
 import { Mode } from "./Mode";
 import { BasicCar } from "./BasicCar";
+import { BARRIER_RAYCAST_LAYER } from "./RaceTrack";
 
 const carBodyMaterial = new THREE.MeshNormalMaterial();
 const carWheelMaterial = new THREE.MeshPhongMaterial({
@@ -10,9 +11,10 @@ const carWheelMaterial = new THREE.MeshPhongMaterial({
 
 export class PlayMode implements Mode {
     gameWorld: GameWorld;
-    car: BasicCar;
+    car?: BasicCar;
     keyupHandler: (e: any) => void;
     keydownHandler: (e: any) => void;
+    arrowHelpers: THREE.Mesh[];
 
     constructor(gameWorld) {
         this.gameWorld = gameWorld;
@@ -39,7 +41,7 @@ export class PlayMode implements Mode {
                     SPACE = true;
                     break;
             }
-            this.car.applyInput(W, A, S, D, SPACE);
+            this.car?.applyInput(W, A, S, D, SPACE);
         };
         this.keyupHandler = (e) => {
             switch (e.key) {
@@ -59,8 +61,28 @@ export class PlayMode implements Mode {
                     SPACE = false;
                     break;
             }
-            this.car.applyInput(W, A, S, D, SPACE);
+            this.car?.applyInput(W, A, S, D, SPACE);
         };
+    }
+
+    update(delta: number) {
+        this.car?.getRays().forEach((ray, idx) => {
+            ray.layers.set(BARRIER_RAYCAST_LAYER);
+            const intersects = ray.intersectObjects(
+                this.gameWorld.scene.children
+            );
+            let distance = 1.0;
+            intersects.forEach((sect) => {
+                if (sect.distance < distance) distance = sect.distance;
+            });
+            const arrowHelper = this.arrowHelpers[idx];
+            arrowHelper.setLength(distance, 0.05, 0.03);
+            arrowHelper.setDirection(ray.ray.direction);
+            arrowHelper.position.x = ray.ray.origin.x;
+            arrowHelper.position.y = ray.ray.origin.y;
+            arrowHelper.position.z = ray.ray.origin.z;
+            arrowHelper.updateMatrix();
+        });
     }
 
     activate() {
@@ -76,12 +98,29 @@ export class PlayMode implements Mode {
         );
         this.gameWorld.addGameObject(this.car);
 
+        this.arrowHelpers = this.car.getRays().map((ray) => {
+            const dir = ray.ray.direction;
+            dir.normalize();
+            const origin = ray.ray.origin;
+            const length = dir.length();
+            const hex = 0xffff00;
+            const arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
+            this.gameWorld.scene.add(arrowHelper);
+            return arrowHelper;
+        });
+
         document.addEventListener("keydown", this.keydownHandler);
         document.addEventListener("keyup", this.keyupHandler);
     }
 
     deactivate() {
         this.gameWorld.removeGameObject(this.car);
+        delete this.car;
+
+        this.arrowHelpers.forEach((arrowHelper) => {
+            this.gameWorld.scene.remove(arrowHelper);
+        });
+        this.arrowHelpers = [];
 
         document.removeEventListener("keydown", this.keydownHandler);
         document.removeEventListener("keyup", this.keyupHandler);
--- src/lib/ThreeCanvas.svelte
+++ src/lib/ThreeCanvas.svelte
@@ -8,9 +8,6 @@
     import { RaceTrack } from "./RaceTrack";
     import { gameWorldStore } from "./stores";
 
-    const BARRIER_RAYCAST_LAYER = 4;
-    const BARRIER_COLLISION_FILTER_GROUP = 16;
-
     let canvas;
     /** @type {GameWorld} */
     let gameWorld;
@@ -44,39 +41,6 @@
         ];
         let road = new RaceTrack(roadPoints, 1.0);
         gameWorld.addGameObject(road);
-
-        const arrowHelpers = car.getRays().map((ray) => {
-            const dir = ray.ray.direction;
-            dir.normalize();
-            const origin = ray.ray.origin;
-            const length = dir.length();
-            const hex = 0xffff00;
-            const arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
-            gameWorld.scene.add(arrowHelper);
-            return arrowHelper;
-        });
-
-        const applyInput = () => {
-            car.getRays().forEach((ray, idx) => {
-                ray.layers.set(BARRIER_RAYCAST_LAYER);
-                const intersects = ray.intersectObjects(
-                    gameWorld.scene.children
-                );
-                let distance = 1.0;
-                intersects.forEach((sect) => {
-                    if (sect.distance < distance) distance = sect.distance;
-                });
-                const arrowHelper = arrowHelpers[idx];
-                arrowHelper.setLength(distance, 0.05, 0.03);
-                arrowHelper.setDirection(ray.ray.direction);
-                arrowHelper.position.x = ray.ray.origin.x;
-                arrowHelper.position.y = ray.ray.origin.y;
-                arrowHelper.position.z = ray.ray.origin.z;
-            });
-
-            window.requestAnimationFrame(applyInput);
-        };
-        window.requestAnimationFrame(applyInput);
     });
 </script>
 
