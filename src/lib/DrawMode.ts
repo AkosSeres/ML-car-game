6d20b7ed42fcd4e9161e25bb4ab6567d55ae4eda
AkosSeres <seresakos2000@gmail.com>
2022-10-31 20:05:53 +0100

    Move prespective change functionality to draw mode

--- src/lib/DrawMode.ts
+++ src/lib/DrawMode.ts
@@ -1,5 +1,6 @@
 import { GameWorld } from "./GameWorld";
 import { Mode } from "./Mode";
+import * as THREE from "three";
 
 export class DrawMode implements Mode {
     gameWorld: GameWorld;
@@ -9,10 +10,20 @@ export class DrawMode implements Mode {
     }
 
     activate() {
-        // TODO
+        this.gameWorld.controls.maxPolarAngle = 0;
+        this.gameWorld.controls.minAzimuthAngle = 0;
+        this.gameWorld.controls.maxAzimuthAngle = 0;
+        this.gameWorld.controls.minDistance = 8;
+        this.gameWorld.controls.maxDistance = 8;
+        this.gameWorld.controls.target = new THREE.Vector3(0, 0, 0);
     }
 
     deactivate() {
-        // TODO
+        this.gameWorld.controls.maxPolarAngle = Math.PI;
+        this.gameWorld.controls.minAzimuthAngle = Infinity;
+        this.gameWorld.controls.maxAzimuthAngle = Infinity;
+        this.gameWorld.controls.minDistance = 0;
+        this.gameWorld.controls.maxDistance = 50;
+        this.gameWorld.controls.reset();
     }
 }
