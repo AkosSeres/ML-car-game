4a1722278eb03a25c1545abfab1383763d4327a4
AkosSeres <seresakos2000@gmail.com>
2022-11-02 00:16:48 +0100

    Modify tab switcher style

--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -44,8 +44,8 @@
             on:click={() => setActiveTab(i)}
             type="button"
             class="transition-all grow py-2 px-4 text-md {i === 0
-                ? 'rounded-l-lg'
-                : ''} {i === titles.length - 1 ? 'rounded-r-lg' : ''} {i ===
+                ? 'rounded-bl-2xl'
+                : ''} {i === titles.length - 1 ? 'rounded-br-2xl' : ''} {i ===
             activeTab
                 ? 'font-bold text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800'
                 : 'font-medium border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white text-gray-900 bg-white'} focus:z-10"
