e23a16562ddc7094df5eaf22af2650f0a981789d
AkosSeres <seresakos2000@gmail.com>
2022-11-05 21:49:09 +0100

    Implement mutation and crossover

--- src/lib/TrainMode.ts
+++ src/lib/TrainMode.ts
@@ -37,35 +37,7 @@ export class TrainMode implements Mode {
     generatePopulation() {
         this.population = [];
         for (let i = 0; i < this.populationSize; i++) {
-            const car = new BasicCar(
-                this.gameWorld.raceTrack.startX,
-                0.4,
-                this.gameWorld.raceTrack.startZ,
-                carBodyMaterial,
-                carWheelMaterial,
-                5,
-                0.25,
-                0.1
-            );
-            car.rotateY(this.gameWorld.raceTrack.startRotation);
-            this.gameWorld.addGameObject(car);
-            const model = tf.sequential({
-                layers: [
-                    tf.layers.dense({ inputShape: [this.inputSize], units: this.hiddenLayerSize, activation: 'tanh' }),
-                    tf.layers.dense({ units: outputSize, activation: 'softmax' }),
-                ]
-            });
-            model.compile({
-                optimizer: 'adam', // or 'sgd'
-                loss: 'binaryCrossentropy', // because we can have multiple outputs 1
-                // metrics: ['accuracy'],
-            });
-            model.weights.forEach(w => {
-                const newVals = tf.randomNormal(w.shape);
-                // w.val is an instance of tf.Variable
-                w.val.assign(newVals);
-            });
-            this.population.push({ car, model, fitness: 0 });
+            this.population.push(this.generateAgent());
         }
     }
 
@@ -83,7 +55,9 @@ export class TrainMode implements Mode {
         if (this.isTraining) {
             this.timeLeft -= delta;
             if (this.timeLeft <= 0) {
-                this.stopTraining();
+                this.doRankingAndCrossover();
+                this.timeLeft = this.maxRunTime;
+                this.resetCars();
             }
         }
 
@@ -94,7 +68,45 @@ export class TrainMode implements Mode {
             const result = element.model.predict(tf.tensor(input)) as tf.Tensor;
             const WASDSPACE = result.arraySync()[0].map(d => d >= 0.5);
             element.car.applyInput(...WASDSPACE);
+
+            // const carPos = element.car.getPosition();
+            // element.fitness = this.gameWorld.raceTrack.amountCompleted(carPos.x, carPos.z);
         });
+    }
+
+    doRankingAndCrossover() {
+        this.population.forEach((element) => {
+            const carPos = element.car.getPosition();
+            element.fitness = this.gameWorld.raceTrack.amountCompleted(carPos.x, carPos.z);
+        });
+        this.population.sort((a, b) => b.fitness - a.fitness);
+
+        const crossoverRate = 0.25;
+        for (let i = Math.round(this.population.length / 2); i < this.population.length; i++) {
+            if (Math.random() >= crossoverRate) continue;
+            const parent1 = this.population[Math.floor(Math.random() * this.population.length / 2)];
+            const parent2 = this.population[Math.floor(Math.random() * this.population.length / 2)];
+            const child = this.population[i];
+            for (let wIdx = 0; wIdx < child.model.weights.length; wIdx++) {
+                const w = child.model.weights[wIdx];
+                const w1 = parent1.model.weights[wIdx];
+                const w2 = parent2.model.weights[wIdx];
+                const r = Math.random();
+                const newVals = tf.tidy(() => {
+                    return w1.val.mul(r).add(w2.val.mul(1 - r));
+                });
+                w.val.assign(newVals);
+            }
+        }
+
+        const mutationRate = 0.05;
+        for (let i = 0; i < this.population.length; i++) {
+            if (Math.random() >= mutationRate) continue;
+            this.population[i].model.weights.forEach(w => {
+                const diff = tf.randomNormal(w.shape, 0, 0.01);
+                w.val.assign(w.val.add(diff));
+            });
+        }
     }
 
     activate() {
@@ -103,5 +115,57 @@ export class TrainMode implements Mode {
 
     deactivate() {
         // TODO
+    }
+
+    resetCars() {
+        this.population.forEach((element) => {
+            this.gameWorld.removeGameObject(element.car);
+            const car = new BasicCar(
+                this.gameWorld.raceTrack.startX,
+                0.4,
+                this.gameWorld.raceTrack.startZ,
+                carBodyMaterial,
+                carWheelMaterial,
+                5,
+                0.25,
+                0.1
+            );
+            car.rotateY(this.gameWorld.raceTrack.startRotation);
+            this.gameWorld.addGameObject(car);
+            element.car = car;
+        });
+    }
+
+    generateAgent() {
+        const car = new BasicCar(
+            this.gameWorld.raceTrack.startX,
+            0.4,
+            this.gameWorld.raceTrack.startZ,
+            carBodyMaterial,
+            carWheelMaterial,
+            5,
+            0.25,
+            0.1
+        );
+        car.rotateY(this.gameWorld.raceTrack.startRotation);
+        this.gameWorld.addGameObject(car);
+        const model = tf.sequential({
+            layers: [
+                tf.layers.dense({ inputShape: [this.inputSize], units: this.hiddenLayerSize, activation: 'tanh' }),
+                tf.layers.dense({ units: outputSize, activation: 'softmax' }),
+            ]
+        });
+        model.compile({
+            optimizer: 'adam', // or 'sgd'
+            loss: 'binaryCrossentropy', // because we can have multiple outputs 1
+            // metrics: ['accuracy'],
+        });
+        model.weights.forEach(w => {
+            // if (!w.name.includes("bias")) return;
+            const newVals = tf.randomNormal(w.shape);
+            // w.val is an instance of tf.Variable
+            w.val.assign(newVals);
+        });
+        return { car, model, fitness: 0 };
     }
 }
